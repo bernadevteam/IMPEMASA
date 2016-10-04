71520c9659fba5e944b53dba2317a635aadb840f
joviedo <joviedo@DESKTOP-OKNPP63>
2016-10-04 11:29:19 -0400

    Add project files.

---
+++ IMPEMASA/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace IMPEMASA.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
